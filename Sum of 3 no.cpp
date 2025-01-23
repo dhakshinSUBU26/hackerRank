8d058fc2df073bdd64a87376bed42785c9a5a297
Subramanian S <dhakshineaswar@gmail.com>
2025-01-23 22:29:54 +0530

    Create Sum of 3 no.cpp

---
+++ Sum of 3 no.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a,b,c; /* declaring integer and sum */
+    int sum; 
+    cin >> a; /* getting inputs*/
+    cin >> b;
+    cin >> c;
+    sum = a+b+c; /* operation*/
+    cout << sum << endl; /* displaying output*/
+    return 0;
+}
